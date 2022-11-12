8c997bdc6524c9d00842d95d125a3d38843c3ba6
carollinemv <106552762+carollinemv@users.noreply.github.com>
2022-11-12 11:47:28 -0300

    Add files via upload

--- FrontWeek/js/scripts.js
+++ FrontWeek/js/scripts.js
@@ -3,14 +3,27 @@ const menuBtn = document.querySelector("#menu");
 const closeMenuBtn = document.querySelector("#close-menu");
 const menu = document.querySelector("#mobile-navbar");
 
+const desktopLinks = document.querySelectorAll("#navbar a");
+const mobileLinks = document.querySelectorAll("#mobile-navbar a");
+const allLinks = [...desktopLinks, ...mobileLinks];
+
+
 
 //Funções
+function smoothScroll(e) {
+    e.preventDefault();
+}
+
 
 // Eventos
 [menuBtn, closeMenuBtn].forEach((btn) =>{
     btn.addEventListener("click",(e) => {
         menu.classList.toggle("menu-active");
     });
+});
+
+allLinks.forEach((link) => {
+    link.addEventListener("click", smoothScroll);
 });
 
 
