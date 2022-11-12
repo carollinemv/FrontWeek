141e25566352ac4302aa2f731fb63e37cca59415
carollinemv <106552762+carollinemv@users.noreply.github.com>
2022-11-12 12:54:50 -0300

    Add files via upload

--- FrontWeek/js/scripts.js
+++ FrontWeek/js/scripts.js
@@ -7,7 +7,7 @@ const desktopLinks = document.querySelectorAll("#navbar a");
 const mobileLinks = document.querySelectorAll("#mobile-navbar a");
 const allLinks = [...desktopLinks, ...mobileLinks];
 
-const slide = document.querySelectorAll(".banner");
+const slides = document.querySelectorAll(".banner");
 const dots = document.querySelectorAll(".dot");
 let slideIndex = 0;
 
@@ -37,6 +37,7 @@ function showSlides() {
 
     for(let i = 0; i < showSlides.length; i++){
         slides[i].classList.remove("active");
+        dots[i].classList.remove("active");
     }
 
     slideIndex++
@@ -44,6 +45,11 @@ function showSlides() {
     if(slideIndex > slides.length) {
         slideIndex = 1;
     }
+
+    slides[slideIndex - 1].classList.add("active");
+    dots[slideIndex - 1].classList.add("active");
+
+    setTimeout(showSlides, 3000);
 }
 
 
